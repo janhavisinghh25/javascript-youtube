b97495bc5d6e2ab4c5a778ed15cb2d70186c3eca
janhavisinghh25 <singhjanhavivns@gmail.com>
2024-07-27 10:39:45 +0000

    container config

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "janhavisingh@gmail.com"
+var accountPassword = "12345"
+accountCity = "varanasi"
+
+//accountId = 18// not allowed
+accountEmail = "janhavi@gmail.com"
+accountPassword= "989898"
+accountCity= "bangalore"
+
+//console.log("accountId");
+
+console.table([accountEmail,accountPassword,accountCity])
