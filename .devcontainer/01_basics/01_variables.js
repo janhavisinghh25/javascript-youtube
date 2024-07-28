91d5ec0d3421c6e64c78c74b21663f931041d417
janhavisinghh25 <singhjanhavivns@gmail.com>
2024-07-28 19:24:19 +0530

    discussion on variables

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -1,13 +1,22 @@
 const accountId = 144553
 let accountEmail = "janhavisingh@gmail.com"
-var accountPassword = "12345"
+var accountPassword = "9333"
 accountCity = "varanasi"
+let accountState;
 
-//accountId = 18// not allowed
+// accountId = 98 // not allowed
+
+
 accountEmail = "janhavi@gmail.com"
-accountPassword= "989898"
-accountCity= "bangalore"
+accountPassword = "98989898"
+accountCity = "Bengaluru"
 
-//console.log("accountId");
+console.log(accountId);
 
-console.table([accountEmail,accountPassword,accountCity])
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
