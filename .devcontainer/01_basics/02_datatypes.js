553777b8d1a5653e8e985306791e99b46ed1bebf
janhavisinghh25 <singhjanhavivns@gmail.com>
2024-07-28 20:23:39 +0530

    discussion on datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("3 + 3")// we are using nodejs not browser
+
+console.log(3 + 3) 
+
+// code readability should be high
+
+console.log("janhavi")
+
+let name = "janhavi"
+let age = 19
+let isLoggedIn = false
+
+
+// number 2 to power 53
+// bigint 
+// string = ""
+// boolean = true/false
+//null = stand alone value 
+// undefined = 
+// symbol = unique
+
+
+//object = 
+ console.log(typeof "undefined"); //undefined
+
+ console.log(typeof null); //object
+
+
