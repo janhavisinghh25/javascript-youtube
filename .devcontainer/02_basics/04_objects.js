20a2c087399d4b508d252fb08516ace4039f93ea
janhavisinghh25 <singhjanhavivns@gmail.com>
2024-08-09 08:28:03 +0000

    json talk

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -46,10 +46,44 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi ",
+    price: "999",
+    courseInstructor : "janhavi"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+
+//console.log(courseInstructor);
+console.log(Instructor);
+
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(comany = "janhavi")
+
+// {
+//     "name" : "janhavi",
+//     "coursename" : "js in hindi",
+//   "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
